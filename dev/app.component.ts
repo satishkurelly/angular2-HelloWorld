9d7a3fa99e2fb28b8893a96db7a87ffff31a8228
pa4506 <satish.flex07@gmail.com>
2016-07-08 21:18:08 -0400

    changes to app component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
+        <h1>Welcome to the Angular 2 World</h1>
         <p>Hello World!!</p>
        
     `,
